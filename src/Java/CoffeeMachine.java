a504b5ce5322ea5404ac78931f5699fa8a6ae47e
kevin_k <kkemp1984@gmail.com>
2021-03-12 11:16:10 -0600

    finished with class creation

--- src/Java/CoffeeMachine.java
+++ src/Java/CoffeeMachine.java
@@ -1,5 +1,42 @@
 public class CoffeeMachine {
     public static void main(String[] args) {
+        CoffeeTouchscreenAdapter adapter = new CoffeeTouchscreenAdapter(new OldCoffeeMachine());
 
+        adapter.chooseFirstSelection();
+        adapter.chooseSecondSelection();
+    }
+}
+
+interface CoffeeMachineInterface {
+
+    void chooseFirstSelection();
+    void chooseSecondSelection();
+}
+
+class CoffeeTouchscreenAdapter implements CoffeeMachineInterface {
+
+    OldCoffeeMachine machine;
+
+    public CoffeeTouchscreenAdapter(OldCoffeeMachine machine){
+        this.machine = machine;
+    }
+
+    public void chooseFirstSelection() {
+        this.machine.selectionA();
+    }
+
+    public void chooseSecondSelection() {
+        this.machine.selectionB();
+    }
+}
+
+class OldCoffeeMachine {
+
+    void selectionA(){
+        System.out.println("selected A");
+    }
+
+    void selectionB(){
+        System.out.println("selected B");
     }
 }
